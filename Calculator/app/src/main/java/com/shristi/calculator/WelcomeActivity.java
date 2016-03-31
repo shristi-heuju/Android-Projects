5a3e5b7be2f920490014944eaf6c78642265a2b8
Shristi Heuju <shristi_heuju@yahoo.com>
2016-03-31 22:12:44 +0545

    Calculator: Welcome page design completed

---
+++ Calculator/app/src/main/java/com/shristi/calculator/WelcomeActivity.java
@@ -0,0 +1,28 @@
+package com.shristi.calculator;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+        Button btnOpenCalculator = (Button) findViewById(R.id.btnOpenCalculator);
+
+        btnOpenCalculator.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(WelcomeActivity.this, MainActivity.class);
+                startActivity(intent);
+
+            }
+        });
+    }
+}
